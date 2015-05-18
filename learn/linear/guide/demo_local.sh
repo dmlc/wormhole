666dd9a63b7719d0e174120938e38d6d8a6bf537
Ubuntu <ubuntu@ip-172-30-0-221.ec2.internal>
2015-05-18 20:36:29 +0000

    update demo

--- learn/linear/guide/demo_local.sh
+++ learn/linear/guide/demo_local.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-../../../dmlc-core/tracker/dmlc_local.py \
-    -n 2 -s 1 ../build/async_sgd \
-    train_data=\\\"../../data/agaricus.txt.train\\\" \
-    val_data=\\\"../../data/agaricus.txt.test\\\"
+../../../dmlc-core/tracker/dmlc_local.py -n 2 -s 1 ../build/async_sgd demo.conf
