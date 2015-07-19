bb45bb8b70e8eca2cbd352cacd61c3e73d2acaf4
muli <muli@cs.cmu.edu>
2015-07-19 04:49:06 +0000

    update zmq version

--- make/deps.mk
+++ make/deps.mk
@@ -41,8 +41,8 @@ protobuf: | ${DEPS_PATH}/include/google/protobuf/message.h
 # zmq
 
 ${DEPS_PATH}/include/zmq.h:
-	$(eval FILE=zeromq-4.1.0-rc1.tar.gz)
-	$(eval DIR=zeromq-4.1.0)
+	$(eval FILE=zeromq-4.1.2.tar.gz)
+	$(eval DIR=zeromq-4.1.2)
 	rm -rf $(FILE) $(DIR)
 	wget $(URL)/$(FILE) && tar -zxf $(FILE)
 	cd $(DIR) && ./configure -prefix=$(DEPS_PATH) --with-libsodium=no --with-libgssapi_krb5=no && $(MAKE) && $(MAKE) install
