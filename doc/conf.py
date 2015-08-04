5dd419a3fcae0a8c4a3f892502874978e90580c6
muli <muli@cs.cmu.edu>
2015-08-03 23:29:34 -0400

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'wormhole'
-copyright = u'2015, Mu Li'
-author = u'Mu Li'
+copyright = u'2015, wormhole developers'
+author = u'wormhole developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -108,7 +108,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -223,7 +223,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'wormhole.tex', u'wormhole Documentation',
-   u'Mu Li', 'manual'),
+   u'wormhole developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
