c35555873c121211bc91cbb5e6debfa35a36e161
muli <muli@cs.cmu.edu>
2015-08-10 23:53:46 -0400

    simplified difacto

--- learn/difacto/loss.h
+++ learn/difacto/loss.h
@@ -10,7 +10,7 @@ namespace difacto {
 /**
  * \brief the loss function
  */
-template <typename V>
+template <typename T>
 class Loss {
  public:
   /**
@@ -22,30 +22,22 @@ class Loss {
    * @param conf difacto conf
    */
   Loss(const RowBlock<unsigned>& data,
-       const std::vector<V>& model,
+       const std::vector<T>& model,
        const std::vector<int>& model_siz,
        const Config& conf) {
     nt_ = conf.num_threads();
 
-    Config cf; cf.add_embedding()->set_dim(0);
-    for (int i = 0; i < conf.embedding_size(); ++i) {
-      if (conf.embedding(i).dim() > 0) {
-        cf.add_embedding()->CopyFrom(conf.embedding(i));
-      }
-    }
+    // init w
+    w.Load(0, data, model, model_siz);
 
-    int n = cf.embedding_size();
-    data_.resize(n);
-
-    for (int i = 0; i < n; ++i) {
-      const auto& eb = cf.embedding(i);
-      data_[i].Load(eb.dim(), data, model, model_siz);
-      if (i > 0) CHECK_GT(data_[i].dim, data_[i-1].dim);
-
-      data_[i].dropout = eb.dropout();
-      data_[i].grad_clipping = eb.grad_clipping();
-      data_[i].grad_normalization = eb.grad_normalization();
-    }
+    // init V
+    if (conf.embedding_size() == 0) return;
+    const auto& cf = conf.embedding(0);
+    if (cf.dim() == 0) return;
+    V.Load(cf.dim(), data, model, model_siz);
+    V.dropout            = cf.dropout();
+    V.grad_clipping      = cf.grad_clipping();
+    V.grad_normalization = cf.grad_normalization();
   }
 
   ~Loss() { }
@@ -61,36 +53,32 @@ class Loss {
   void Evaluate(Progress* prog) {
 
     // py = X * w
-    auto& d = data_[0];
-    py_.resize(d.X.size);
-    SpMV::Times(d.X, d.w, &py_, nt_);
+    py_.resize(w.X.size);
+    SpMV::Times(w.X, w.weight, &py_, nt_);
 
-    BinClassEval<V> eval(d.X.label, py_.data(), py_.size(), nt_);
+    BinClassEval<T> eval(w.X.label, py_.data(), py_.size(), nt_);
     prog->objv_w() = eval.LogitObjv();
 
     // py += .5 * sum((X*V).^2 - (X.*X)*(V.*V), 2);
-    for (size_t k = 1; k < data_.size(); ++k) {
-      auto& d = data_[k];
-      if (d.w.empty()) continue;
-
+    if (!V.weight.empty()) {
       // tmp = (X.*X)*(V.*V)
-      std::vector<V> vv = d.w;
+      std::vector<T> vv = V.weight;
       for (auto& v : vv) v *= v;
-      CHECK_EQ(vv.size(), d.pos.size() * d.dim);
-      std::vector<V> xxvv(d.X.size * d.dim);
-      SpMM::Times(d.XX, vv, &xxvv, nt_);
+      CHECK_EQ(vv.size(), V.pos.size() * V.dim);
+      std::vector<T> xxvv(V.X.size * V.dim);
+      SpMM::Times(V.XX, vv, &xxvv, nt_);
 
-      // d.XV = X*V
-      d.XV.resize(xxvv.size());
-      SpMM::Times(d.X, d.w, &d.XV, nt_);
+      // V.XV = X*V
+      V.XV.resize(xxvv.size());
+      SpMM::Times(V.X, V.weight, &V.XV, nt_);
 
-      // py += .5 * sum((d.XV).^2 - xxvv)
+      // py += .5 * sum((V.XV).^2 - xxvv)
 #pragma omp parallel for num_threads(nt_)
       for (size_t i = 0; i < py_.size(); ++i) {
-        V* t = d.XV.data() + i * d.dim;
-        V* tt = xxvv.data() + i * d.dim;
-        V s = 0;
-        for (int j = 0; j < d.dim; ++j) s += t[j] * t[j] - tt[j];
+        T* t = V.XV.data() + i * V.dim;
+        T* tt = xxvv.data() + i * V.dim;
+        T s = 0;
+        for (int j = 0; j < V.dim; ++j) s += t[j] * t[j] - tt[j];
         py_[i] += .5 * s;
       }
     }
@@ -99,7 +87,7 @@ class Loss {
     prog->objv()   = eval.LogitObjv();
     prog->auc()    = eval.AUC();
     // prog->copc()   = eval.Copc();
-    prog->new_ex() = d.X.size;
+    prog->new_ex() = w.X.size;
     prog->count()  = 1;
   }
 
@@ -109,74 +97,70 @@ class Loss {
    * grad_w = X' * p;
    * grad_u = X' * diag(p) * X * V  - diag((X.*X)'*p) * V
    */
-  void CalcGrad(std::vector<V>* grad) {
+  void CalcGrad(std::vector<T>* grad) {
     // p = ... (reuse py_)
-    auto& d = data_[0];
-    CHECK_EQ(py_.size(), d.X.size) << "call *evaluate* first";
+    CHECK_EQ(py_.size(), w.X.size) << "call *evaluate* first";
 #pragma omp parallel for num_threads(nt_)
     for (size_t i = 0; i < py_.size(); ++i) {
-      V y = d.X.label[i] > 0 ? 1 : -1;
+      T y = w.X.label[i] > 0 ? 1 : -1;
       py_[i] = - y / ( 1 + exp ( y * py_[i] ));
     }
 
     // grad_w = ...
-    SpMV::TransTimes(d.X, py_, &d.w, nt_);
-    // Normalize(d.w);
+    SpMV::TransTimes(w.X, py_, &w.weight, nt_);
+    w.Save(grad);
 
     // grad_u = ...
-    for (size_t k = 1; k < data_.size(); ++k) {
-      auto& d = data_[k];
-      if (d.w.empty()) continue;
-      int dim = d.dim;
+    if (!V.weight.empty()) {
+      int dim = V.dim;
 
       // xxp = (X.*X)'*p
-      size_t m = d.pos.size();
-      std::vector<V> xxp(m);
-      SpMM::TransTimes(d.XX, py_, &xxp, nt_);
+      size_t m = V.pos.size();
+      std::vector<T> xxp(m);
+      SpMM::TransTimes(V.XX, py_, &xxp, nt_);
 
       // V = - diag(xxp) * V
-      CHECK_EQ(d.w.size(), dim * m);
+      CHECK_EQ(V.weight.size(), dim * m);
 #pragma omp parallel for num_threads(nt_)
       for (size_t i = 0; i < m; ++i) {
-        V* v = d.w.data() + i * dim;
+        T* v = V.weight.data() + i * dim;
         for (int j = 0; j < dim; ++j) v[j] *= - xxp[i];
       }
 
-      // d.XV = diag(p) * X * V
+      // V.XV = diag(p) * X * V
       size_t n = py_.size();
-      CHECK_EQ(d.XV.size(), n * dim);
+      CHECK_EQ(V.XV.size(), n * dim);
 #pragma omp parallel for num_threads(nt_)
       for (size_t i = 0; i < n; ++i) {
-        V* y = d.XV.data() + i * dim;
+        T* y = V.XV.data() + i * dim;
         for (int j = 0; j < dim; ++j) y[j] *= py_[i];
       }
 
-      // V += X' * d.XV
-      SpMM::TransTimes(d.X, d.XV, (V)1, d.w, &d.w, nt_);
+      // V += X' * V.XV
+      SpMM::TransTimes(V.X, V.XV, (T)1, V.weight, &V.weight, nt_);
 
       // some preprocessing
-      if (d.grad_clipping > 0) {
-        V gc = d.grad_clipping;
-        for (V& g : d.w) g = g > gc ? gc : ( g < -gc ? -gc : g);
+      if (V.grad_clipping > 0) {
+        T gc = V.grad_clipping;
+        for (T& g : V.weight) g = g > gc ? gc : ( g < -gc ? -gc : g);
       }
 
-      if (d.dropout > 0) {
-        for (V& g : d.w) {
-          if ((V)rand() / RAND_MAX > 1 - d.dropout) g = 0;
+      if (V.dropout > 0) {
+        for (T& g : V.weight) {
+          if ((T)rand() / RAND_MAX > 1 - V.dropout) g = 0;
         }
       }
-      if (d.grad_normalization) Normalize(d.w);
+      if (V.grad_normalization) Normalize(V.weight);
     }
-
-    for (const auto& d : data_) d.Save(grad);
+    V.Save(grad);
   }
 
-  void Normalize(std::vector<V>& grad) {
-    V norm = 0;
-    for (V g : grad) norm += g * g;
+  void Normalize(std::vector<T>& grad) {
+    T norm = 0;
+    for (T g : grad) norm += g * g;
     if (norm < 1e-10) return;
     norm = sqrt(norm);
-    for (V& g : grad) g = g / norm;
+    for (T& g : grad) g = g / norm;
   }
 
   virtual void Predict(Stream* fo, bool prob_out) {
@@ -193,20 +177,20 @@ class Loss {
   struct Data {
     /// \brief get data and model
     void Load(int d, const RowBlock<unsigned>& data,
-              const std::vector<V>& model,
+              const std::vector<T>& model,
               const std::vector<int>& model_siz) {
       // init pos and w
       std::vector<unsigned> col_map;
       dim = d;
       if (dim == 0) {  // w
         pos.resize(model_siz.size());
-        w.resize(model_siz.size());
+        weight.resize(model_siz.size());
         unsigned p = 0;
         for (size_t i = 0; i < model_siz.size(); ++i) {
           if (model_siz[i] == 0) {
             pos[i] = (unsigned)-1;
           } else {
-            pos[i] = p; w[i] = model[p]; p += model_siz[i];
+            pos[i] = p; weight[i] = model[p]; p += model_siz[i];
           }
         }
         CHECK_EQ((size_t)p, model.size());
@@ -221,12 +205,12 @@ class Loss {
           p += model_siz[i];
         }
         CHECK_EQ((size_t)p, model.size());
-        w.resize(pos.size() * dim);
+        weight.resize(pos.size() * dim);
         for (size_t i = 0; i < pos.size(); ++i) {
-          memcpy(w.data()+i*dim, model.data()+pos[i], dim*sizeof(V));
+          memcpy(weight.data()+i*dim, model.data()+pos[i], dim*sizeof(V));
         }
       }
-      if (w.empty()) return;
+      if (weight.empty()) return;
 
       // init X
       if (dim == 0) {  // w
@@ -240,55 +224,57 @@ class Loss {
             unsigned k = col_map[d];
             if (k > 0) {
               idx.push_back(k-1);
-              if (data.value) val.push_back(data.value[j]);
+              if (data.value) val_.push_back(data.value[j]);
             }
           }
           os.push_back(idx.size());
         }
         X.size = data.size;
         X.offset = BeginPtr(os);
-        X.value = BeginPtr(val);
+        X.value = BeginPtr(val_);
         X.index = BeginPtr(idx);
       }
 
       // init XX
       XX = X;
       if (X.value) {
-        v2.resize(X.offset[X.size]);
-        for (size_t i = 0; i < v2.size(); ++i) v2[i] = X.value[i] * X.value[i];
-        XX.value = BeginPtr(v2);
+        val2_.resize(X.offset[X.size]);
+        for (size_t i = 0; i < val2_.size(); ++i) {
+          val2_[i] = X.value[i] * X.value[i];
+        }
+        XX.value = BeginPtr(val2_);
       }
     }
 
     /// \brief set the gradient
-    void Save(std::vector<V>* grad) const {
-      if (w.empty()) return;
+    void Save(std::vector<T>* grad) const {
+      if (weight.empty()) return;
       int d = dim == 0 ? 1 : dim;
-      CHECK_EQ(w.size(), pos.size()*d);
+      CHECK_EQ(weight.size(), pos.size()*d);
       for (size_t i = 0; i < pos.size(); ++i) {
         if (pos[i] == (unsigned)-1) continue;
-        memcpy(grad->data()+pos[i], w.data()+i*d, d*sizeof(V));
+        memcpy(grad->data()+pos[i], weight.data()+i*d, d*sizeof(V));
       }
     }
 
     int dim;
     RowBlock<unsigned> X, XX;  // XX = X.*X
-    std::vector<V> w;
+    std::vector<T> weight;
     std::vector<unsigned> pos;
 
-    std::vector<V> XV;
-    V dropout = 0;
-    V grad_clipping = 0;
-    V grad_normalization = 0;
+    std::vector<T> XV;
+    T dropout = 0;
+    T grad_clipping = 0;
+    T grad_normalization = 0;
    private:
-    std::vector<V> val, v2;
+    std::vector<T> val_, val2_;
     std::vector<size_t> os;
     std::vector<unsigned> idx;
   };
-  // Data w, V;
-  std::vector<Data> data_;
+  Data w, V;
+  // std::vector<Data> data_;
 
-  std::vector<V> py_;
+  std::vector<T> py_;
   int nt_;  // number of threads
 };
 
